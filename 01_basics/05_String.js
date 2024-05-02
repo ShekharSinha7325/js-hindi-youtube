0c077a2b1505193348f7dc999ce426d036172d8b
Shekhar Kumar Sinha <128055500+ShekharSinha7325@users.noreply.github.com>
2024-05-02 17:50:04 +0000

    String In Js

---
+++ 01_basics/05_String.js
@@ -0,0 +1,53 @@
+const Name = "hitesh"
+const repoCount = 50
+
+// console.log(Name + repoCount + "Value");
+
+console.log(`Hello my Name is ${Name} and  my repo count is ${repoCount}`);
+
+const gameName = new  String ('Shekhar-Sinha')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+ console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+//console.log(gameName.toLowerCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('h'));
+
+//  const newString = gameName.substring(0,4)
+//  console.log(newString);
+
+//  const anotherString = gameName.slice(-13, -2)
+//  console.log(anotherString);
+
+
+//  const newStringOne = "  Shekhar   "
+//  console.log(newStringOne);
+//  console.log(newStringOne.trim());
+
+//  console.log(newStringOne.trimStart());
+//  console.log(newStringOne.trimEnd());
+
+
+const url = "https://shekhar.com/shekhar%20sinha"
+
+console.log(url.replace('%20', '-'));
+console.log(url.includes('%20'));
+
+
+
+console.log(gameName.split(','));
+console.log(gameName.startsWith('rahul'));
+console.log(gameName.endsWith('Sinha'));
+
+
+
+
+
+
+
+
+
+
