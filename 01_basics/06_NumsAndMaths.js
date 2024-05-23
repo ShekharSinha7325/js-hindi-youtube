51718aec4d9f67bbe14589c6d89145780b312011
Shekhar Kumar Sinha <128055500+ShekharSinha7325@users.noreply.github.com>
2024-05-23 08:37:30 +0000

    Maths And Nums

---
+++ 01_basics/06_NumsAndMaths.js
@@ -0,0 +1,34 @@
+const score = 400
+// console.log(score);
+
+
+const blance = new Number(100)
+// console.log(blance);
+// console.log(blance.toString().length);
+// console.log(blance.toFixed(2));
+
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++++Maths+++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4));
+// console.log(Math.round(50.6));
+// console.log(Math.ceil(30.2));
+// console.log(Math.floor(30.9));
+// console.log(Math.min(4 , 6, 7, 9, 10));
+// console.log(Math.max(4 , 6, 7, 9, 10));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min +1))+ min);
